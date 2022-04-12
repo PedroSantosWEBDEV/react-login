0cec53298bdfcb0f7801f2de0beebc5aa4d487ab
Pedro Santos <54372631+PedroSantosWEBDEV@users.noreply.github.com>
2022-04-12 18:41:47 -0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const db = mysql.createPool({
   host: "localhost",
   user: "root",
   password: "",
-  database: "crudgame",
+  database: "login",
 });
 
 app.use(express.json());
@@ -19,62 +19,51 @@ app.post("/register", (req, res) => {
   const { email } = req.body;
   const { password } = req.body;
 
-  let mysql = "INSERT INTO games ( firstname, lastname, email, password) VALUES (?, ?, ?, ?)";
+  let mysql = "INSERT INTO usuario ( firstname, lastname, email, password) VALUES (?, ?, ?, ?)";
   db.query(mysql, [firstname, lastname, email, password], (err, result) => {
     res.send(result);
   });
 });
 
-app.post("/search", (req, res) => {
-  const { name } = req.body;
-  const { cost } = req.body;
-  const { category } = req.body;
+// app.post("/search", (req, res) => {
+//   const { firstname } = req.body;
+//   const { lastname } = req.body;
+//   const { email } = req.body;
 
-  let mysql =
-    "SELECT * from games WHERE name = ? AND cost = ? AND category = ?";
-  db.query(mysql, [name, cost, category], (err, result) => {
-    if (err) res.send(err);
-    res.send(result);
-  });
-});
+//   let mysql =
+//     "SELECT * from usuario WHERE firstname = ? AND lastname = ? AND email = ?";
+//   db.query(mysql, [firstname, lastname, email], (err, result) => {
+//     if (err) res.send(err);
+//     res.send(result);
+//   });
+// });
 
-app.get("/getCards", (req, res) => {
-  let mysql = "SELECT * FROM games";
-  db.query(mysql, (err, result) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.send(result);
-    }
-  });
-});
+// app.put("/edit", (req, res) => {
+//   const { id } = req.body;
+//   const { name } = req.body;
+//   const { cost } = req.body;
+//   const { category } = req.body;
+//   let mysql = "UPDATE usuario SET name = ?, cost = ?, category = ? WHERE id = ?";
+//   db.query(mysql, [name, cost, category, id], (err, result) => {
+//     if (err) {
+//       res.send(err);
+//     } else {
+//       res.send(result);
+//     }
+//   });
+// });
 
-app.put("/edit", (req, res) => {
-  const { id } = req.body;
-  const { name } = req.body;
-  const { cost } = req.body;
-  const { category } = req.body;
-  let mysql = "UPDATE games SET name = ?, cost = ?, category = ? WHERE id = ?";
-  db.query(mysql, [name, cost, category, id], (err, result) => {
-    if (err) {
-      res.send(err);
-    } else {
-      res.send(result);
-    }
-  });
-});
-
-app.delete("/delete/:id", (req, res) => {
-  const { id } = req.params;
-  let mysql = "DELETE FROM games WHERE id = ?";
-  db.query(mysql, id, (err, result) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.send(result);
-    }
-  });
-});
+// app.delete("/delete/:id", (req, res) => {
+//   const { id } = req.params;
+//   let mysql = "DELETE FROM usuario WHERE id = ?";
+//   db.query(mysql, id, (err, result) => {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       res.send(result);
+//     }
+//   });
+// });
 
 app.listen(3001, () => {
   console.log("rodando na porta 3001");
